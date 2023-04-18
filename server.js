a6c86b5d85be9bf532c49ee1f8773e81cac84956
riccaralv <riccaralv@gmail.com>
2023-04-18 13:07:42 +0200

    add userRoute

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import usersRoute from './routes/usersRoute.js';
 dotenv.config();
 
+// CREATE EXPRESS SERVER
 const app = express();
+
+// PORT SETTING
 const PORT = process.env.PORT || 3000;
 
-// connecting DB
+// MONGOOSE CONNECTION
 mongoose
   .connect(process.env.URI)
   .then(() => console.log('Connection to DB done'))
   .catch((err) => console.log(err.message));
 
-//Middlewares
+// MIDDLEWARES
 app.use(express.json());
 
 // test connection
-app.get('/', (re, res) => {
+app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-// running server
+// ROUTES
+app.use('/users', usersRoute);
+
+/**
+ * Do we add photos of the recipes? If yes, we need 'read stream'
+ */
+
+// RUNNING SERVER
 app.listen(PORT, () => console.log('Server is running on PORT', PORT));
