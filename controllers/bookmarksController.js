9854d46b2334286d39952a51f78ba6785455eab4
Darko <dj_mamlyooo@hotmail.com>
2023-04-18 13:43:36 +0200

    bookmarksController added

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,23 +1,32 @@
 import BookmarkedCollection from "../models/bookmarkSchema.js"
+import RecipeCollection from '../models/recipeSchema.js';
 
 
-export const getAllRecipes = async (req, res) => {
+export const getAllBookmarks = async (req, res) => {
     try{
         const bookmarks = await BookmarkedCollection.find()
         .populate("userId, "lastName email")
-        .populate("recipes", "title price");
+        .populate("recipes", "Name url");
         res.json({succes:true, data: recipes});
     }catch(err){
         res.json({succes:false, message: err.message});
     }
 };
 
-export const addNewRecipe = async (req, res, next) =>{
+export const addNewBookmark = async (req, res, next) =>{
     try{
         const recipe = new BookmarkedCollection(req.body);
         await recipe.save()
         res.json({ success:true, data: recipe })
-    }
     }catch(err){
         res.json({ success:false, message: err.message});
+    };
+}
+
+export const getSingleBookmark = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const recipe = await BookmarkedCollection.findById(id);
+        
     }
+}   catch(err){}
