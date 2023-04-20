5c86dbeee40793e1bcdf0135773da063231cc21c
riccaralv <riccaralv@gmail.com>
2023-04-20 15:13:57 +0200

    add buttons to recipe page and functionality

--- frontend/src/components/RecipesPage.jsx
+++ frontend/src/components/RecipesPage.jsx
@@ -1,43 +1,60 @@
+import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const URL = 'http://localhost:3000/recipes';
 
 function RecipesPage() {
-	// states
-	const [recipes, setRecipes] = useState([]);
+  // states
+  const [recipes, setRecipes] = useState([]);
 
-	// actions
-	useEffect(() => {
-		fetch(URL)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data.data);
-				setRecipes(data.data);
-			});
-	}, []);
+  // actions
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data);
+        setRecipes(data.data);
+      });
+  }, []);
 
-	const bookmarkRecipe = (recipe) => {
-		console.log('click');
-	};
+  const tryFunction = () => {
+    axios.get('/recipes/:id');
+    console.log('try');
+  };
 
-	// render
-	return (
-		<div>
-			<h1>Recipes</h1>
+  // render
+  return (
+    <div>
+      <h1>Recipes</h1>
 
-			{recipes.map((recipe) => {
-				return (
-					<div key={recipe._id}>
-						<h3>{recipe.Name}</h3>
-						<button onClick={() => bookmarkRecipe(recipe)}>
-							Bookmark
-						</button>
-					</div>
-				);
-			})}
-		</div>
-	);
+      {recipes.map((recipe) => {
+        return (
+          <div
+            key={recipe._id}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              borderBottom: '2px solid tomato',
+              margin: '10px',
+            }}
+          >
+            <h3>{recipe.Name}</h3>
+            <img src={recipe.Image} alt='' />
+            {/* <button onClick={() => bookmarkRecipe(recipe)}>Bookmark</button> */}
+            <div style={{ margin: '5px' }}>
+              {/* <button>Read more</button> */}
+              <Link to={`${recipe._id}`} state={recipe}>
+                <button>See more</button>
+              </Link>
+              <button>Save</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default RecipesPage;
