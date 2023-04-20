69e71b47290c39656930094bd69fd4f8cbb51691
riccaralv <riccaralv@gmail.com>
2023-04-20 16:04:51 +0200

    finish single recipe page

--- frontend/src/components/SingleRecipe.jsx
+++ frontend/src/components/SingleRecipe.jsx
@@ -4,16 +4,26 @@ const SingleRecipe = () => {
   const location = useLocation();
   console.log(location);
   return (
-    <div>
+    <div style={{ textAlign: 'left' }}>
       {location.state && (
         <div>
           <h2>{`${location.state.Author}, by ${location.state.Author}`}</h2>
           <p>{location.state.Description}</p>
           <p>Here you can find all the ingredients:</p>
-
+          <ul>
+            {location.state.Ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
           <p>Follow the instructions:</p>
-
-          {/* <p>{location.state.Ingredients}</p> */}
+          <ol>
+            {location.state.Method.map((method, index) => (
+              <li key={index} style={{ textAlign: 'left' }}>
+                {method}
+              </li>
+            ))}
+          </ol>
+          <img src={location.state.Image} alt='' />
         </div>
       )}
     </div>
